3a6248f90fcfce7adf1c22812fc5cc8c3dfd6388
DrissDW <driss.mallali@yahoo.com>
2023-09-16 16:06:22 +0200

    Importing the Banner component into the Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,14 @@
+import React from 'react';
+import Banner from '../Banner/Banner'; 
+
 function Home() {
-    return <div>Page d'acceuil 🏠</div>
-  }
-  
-  export default Home
+  return (
+    <div className="home">
+      <Banner /> 
+      
+    </div>
+  );
+}
+
+export default Home;
   
