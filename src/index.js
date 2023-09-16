b0a724ee5db7c5eb730dff7b924f40b032e41f48
DrissDW <driss.mallali@yahoo.com>
2023-09-16 16:07:18 +0200

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './styles/index.scss';
-import App from './App';
 import Home from './components/Home/Home';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Error from './components/Error/Error';
+
 
 
 ReactDOM.render(
