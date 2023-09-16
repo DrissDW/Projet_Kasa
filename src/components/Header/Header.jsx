31d551ef2f57b16e8ab56632662b46b66b0c0f45
DrissDW <driss.mallali@yahoo.com>
2023-09-16 14:09:17 +0200

    Creating the Header component and setting up the className

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../styles/Header.scss';
+import LogoPicture from '../../assets/kasa-logo.png';
+
+
 function Header() {
-    return <div>Header</div>
+    return (
+      <header className="header">
+        <div className="header__logo">
+          <img src={LogoPicture} alt="Logo du site kasa" />
+        </div>
+        <nav className="header__nav">
+          <ul>
+            <li>
+              <Link to="/" className="header__nav-link">Accueil</Link>
+            </li>
+            <li>
+              <Link to="/a-propos" className="header__nav-link">A Propos</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
   }
   
   export default Header
