5636feedc91163c4156bd4fd642254e11404cf01
DrissDW <driss.mallali@yahoo.com>
2023-09-16 15:18:44 +0200

    Creating the Footer component and setting up classNames

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,17 @@
+import React from 'react';
+import footerPicture from '../../assets/kasa-logo-Footer.png'
+import '../../styles/Footer.scss';
+
+
 function Footer() {
-    return <div>Footer</div>
+    return (
+      <footer className="footer">
+        <div className="footer__logo">
+        <img src={footerPicture} alt="Logo du Footer" />
+        </div>
+        <p className="footer__rights">© 2020 Kasa. All rights reserved</p>
+      </footer>
+    );
   }
   
   export default Footer
