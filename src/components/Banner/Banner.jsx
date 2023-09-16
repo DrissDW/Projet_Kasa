90a93be8f5d693a15bc2dbfbb4b6889f1a328f2b
DrissDW <driss.mallali@yahoo.com>
2023-09-16 16:02:56 +0200

    Creating the Banner component and setting up ClassNames

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../styles/Banner.scss';
+
+function Banner() {
+    return (
+      <div className="banner">
+        <div className="banner__content">
+          <h1 className="banner__title">Chez vous, partout et ailleurs</h1>
+        </div>
+      </div>
+    );
+  }
+
+  export default Banner;
