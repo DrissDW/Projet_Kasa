95c3b470850c543de81742200088da1374aa5ea9
DrissDW <driss.mallali@yahoo.com>
2023-09-17 11:37:07 +0200

    Header component updated so that the last link clicked remains underlined

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../../styles/Header.scss';
 import LogoPicture from '../../assets/kasa-logo.png';
 
-
 function Header() {
-    return (
-      <header className="header">
-        <div className="header__logo">
-          <img src={LogoPicture} alt="Logo du site kasa" />
-        </div>
-        <nav className="header__nav">
-          <ul>
-            <li>
-              <Link to="/" className="header__nav-link">Accueil</Link>
-            </li>
-            <li>
-              <Link to="/a-propos" className="header__nav-link">A Propos</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-  
-  export default Header
+  const [clickedLink, setClickedLink] = useState(null);
+
+  const handleLinkClick = (link) => {
+    setClickedLink(link);
+  };
+
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <img src={LogoPicture} alt="Logo du site kasa" />
+      </div>
+      <nav className="header__nav">
+        <ul>
+          <li>
+            <Link
+              to="/"
+              className={`header__nav-link ${clickedLink === '/' ? 'clicked' : ''}`}
+              onClick={() => handleLinkClick('/')}
+            >
+              Accueil
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/a-propos"
+              className={`header__nav-link ${clickedLink === '/a-propos' ? 'clicked' : ''}`}
+              onClick={() => handleLinkClick('/a-propos')}
+            >
+              À Propos
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
   
