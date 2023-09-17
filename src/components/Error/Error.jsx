76fe5080f8178202204d14278a24f3ca9efe9bef
DrissDW <driss.mallali@yahoo.com>
2023-09-17 11:57:57 +0200

    Creation of the Error component and implementation of its Scss code

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,6 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../styles/Error.scss';
+
 function Error() {
-    return <div>Page Error </div>
-  }
-  
-  export default Error
+  return (
+    <div className="error">
+      <h1 className="error__title">404</h1>
+      <p className="error__message">Oups! la page que vous demandez n'existe pas.</p>
+      <Link to="/" className="error__link">Retourner sur la page d'accueil</Link>
+    </div>
+  );
+}
+
+export default Error;
+
   
