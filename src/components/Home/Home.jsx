7170ac16adef904e7dfa021ab4315ed5b2d391f1
DrissDW <driss.mallali@yahoo.com>
2023-09-18 15:52:06 +0200

    Adding the Gallery component to the Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner'; 
+import Gallery from '../Gallery/Gallery'
 
 function Home() {
   return (
     <div className="home">
       <Banner /> 
+      <Gallery />
       
     </div>
   );
