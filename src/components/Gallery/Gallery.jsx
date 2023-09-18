0f1d6ec72242f0cf1dd9ed4ff8dc8995fbee5182
DrissDW <driss.mallali@yahoo.com>
2023-09-18 15:51:18 +0200

    Creating a temporary gallery component

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,6 +1,19 @@
+import React from 'react';
+import '../../styles/Gallery.scss';
+
 function Gallery() {
-    return <div>Gallery</div>
-  }
-  
-  export default Gallery
-  
+  return (
+    <div className="gallery">
+      <h1>GALLERY (provisoire)</h1>
+      <div className="logement-container">
+        {Array.from({ length: 6 }, (_, index) => (
+          <div className="logement" key={index}>
+            <p className="logement-title"> Titre du Logement</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Gallery;
