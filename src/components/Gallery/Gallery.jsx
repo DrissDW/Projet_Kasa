ea3ef79c05e7362a53a34c46335a6db87d9b670e
DrissDW <driss.mallali@yahoo.com>
2023-09-21 17:24:08 +0200

    Gallery component updated with fetch request to retrieve the title of each home

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../styles/Gallery.scss';
 
 function Gallery() {
+  const [logements, setLogements] = useState([]);
+
+  useEffect(() => {
+    // requête pour charger le fichier JSON 
+    fetch('/data/logementsList.json')
+      .then((response) => response.json())
+      .then((data) => {
+       
+        setLogements(data);
+      })
+      .catch((error) => {
+        console.error('Une erreur est produite lors du chargement du fichier JSON :', error);
+      });
+  }, []);
+
   return (
     <div className="gallery">
-      <h1>GALLERY (provisoire)</h1>
+      <h1>GALLERY</h1>
       <div className="logement-container">
-        {Array.from({ length: 6 }, (_, index) => (
-          <div className="logement" key={index}>
-            <p className="logement-title"> Titre du Logement</p>
-          </div>
-        ))}
+        {logements.length > 0 ? (
+          logements.map((logement, index) => (
+            <div className="logement" key={index}>
+              <p className="logement-title">{logement.title}</p>
+              
+            </div>
+          ))
+        ) : (
+          <p>Aucun logement n'a été trouvé.</p>
+        )}
       </div>
     </div>
   );
 }
 
 export default Gallery;
+
