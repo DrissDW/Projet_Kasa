a77e9eea6ec182b8b1705bdd9f870d5f8963751d
DrissDW <driss.mallali@yahoo.com>
2023-09-21 17:29:08 +0200

    Updating the Home.jsx file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Banner from '../components/Banner/Banner'; 
-import Gallery from '../components/Gallery/Gallery'
+import Banner from '../components/Banner/Banner.jsx'; 
+import Gallery from '../components/Gallery/Gallery.jsx'
 
 function Home() {
   return (
