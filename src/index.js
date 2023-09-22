fa99f4760505a112177618df721ba93067d04ad9
DrissDW <driss.mallali@yahoo.com>
2023-09-22 18:05:48 +0200

    Added FicheLogement route

--- src/index.js
+++ src/index.js
@@ -3,24 +3,23 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './styles/index.scss';
 import Home from './pages/Home';
+import FicheLogement from './pages/FicheLogement'; 
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Error from './pages/Error.jsx';
 
-// Créez une instance de createRoot
 const root = createRoot(document.getElementById('root'));
 
-// Utilisez la méthode render sur cette instance
 root.render(
   <React.StrictMode>
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/logement/:id" element={<FicheLogement />} /> 
         <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
     </Router>
   </React.StrictMode>
 );
-
