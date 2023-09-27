3f1f072d15de174e367e0fc1fbc388700224148f
DrissDW <driss.mallali@yahoo.com>
2023-09-27 15:43:27 +0200

    Replacing the 404 title with the imported logo

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Error.scss';
+import logo404 from './../assets/logo_error_404.png';
 
 function Error() {
   return (
     <div className="error">
-      <h1 className="error__title">404</h1>
+      <img src={logo404} alt="Logo 404" className="error__logo" />
       <p className="error__message">Oups! la page que vous demandez n'existe pas.</p>
       <Link to="/" className="error__link">Retourner sur la page d'accueil</Link>
     </div>
