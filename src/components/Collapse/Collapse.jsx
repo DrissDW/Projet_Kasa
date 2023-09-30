ff70865aac4f759cde6cda37231b74de641072b8
DrissDW <driss.mallali@yahoo.com>
2023-09-30 16:15:16 +0200

    Update collapse component

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -12,16 +12,20 @@ function Collapse({ title, content }) {
     <div className={`collapse ${isOpen ? 'open' : ''}`}>
       <div className="collapse-header">
         <div className="collapse-title">{title}</div>
-        <button className="collapse-button" onClick={toggleCollapse}>
-          {isOpen ? <span>&#9654;</span> : <span>&#9660;</span>}
+        <button className={`collapse-button ${isOpen ? 'open' : ''}`} onClick={toggleCollapse}>
+          {isOpen ? <span>&#9660;</span> : <span>&#9654;</span>}
         </button>
       </div>
-      {isOpen && <div className="collapse-content">{content}</div>}
+      <div className="collapse-content">
+        {content}
+      </div>
     </div>
   );
 }
 
 export default Collapse;
+
+
 
 
 
