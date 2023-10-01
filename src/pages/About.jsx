734291022c484b02e672a2735628ca7be9863f0a
DrissDW <driss.mallali@yahoo.com>
2023-10-01 18:41:02 +0200

    Adding the Banner component to the About page in a className="banner-about" div

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Collapse from '../components/Collapse/Collapse';
-
+import Banner from '../components/Banner/Banner.jsx';  
 
 function About() {
     const [aboutData, setAboutData] = useState([]);
@@ -15,6 +15,10 @@ function About() {
   
     return (
       <div className="about">
+        <div className="banner-about">
+        <Banner page="about" />
+  
+     </div>
         <div className="accordion">
           {aboutData.map((item) => (
             <Collapse key={item.id} title={item.title} content={item.content} />
