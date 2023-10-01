b32210fea7ee3fa5f613819817decd0b87c582f2
DrissDW <driss.mallali@yahoo.com>
2023-10-01 18:35:40 +0200

    Add page props to Banner component to select banner on Home or About page

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import '../../styles/Banner.scss';
 
-function Banner() {
-    return (
-      <div className="banner">
-        <div className="banner__content">
-          <h1 className="banner__title">Chez vous, partout et ailleurs</h1>
-        </div>
+function Banner({ page }) {
+  return (
+    <div className={`banner-${page}`}>
+      <div className="banner__content">
+        <h1 className="banner__title">Chez vous, partout et ailleurs</h1>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  export default Banner;
+export default Banner;
+
