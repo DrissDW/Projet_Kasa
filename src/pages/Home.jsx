c4f761aca47532c169cd31b62e02db4df88d8ea4
DrissDW <driss.mallali@yahoo.com>
2023-10-02 14:28:35 +0200

    Importing the Home.scss file on the Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../components/Banner/Banner.jsx'; 
 import Gallery from '../components/Gallery/Gallery.jsx'
+import '../styles/Home.scss'; 
 
 function Home() {
   return (
