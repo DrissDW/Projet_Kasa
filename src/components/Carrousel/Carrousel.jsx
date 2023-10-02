d82e60fbea4111e3644f8f9e25e06c60bb02763b
DrissDW <driss.mallali@yahoo.com>
2023-10-02 03:52:32 +0200

    Carousel component optimization

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -14,24 +14,29 @@ function Carrousel({ images }) {
 
   return (
     <div className="carrousel">
-      <div className="carrousel-navigation">
-        {images.length > 1 && (
+      <div className="carrousel-image-container">
+        {images.length > 1 && ( // Condition pour afficher les boutons seulement s'il y a plus d'une image
           <>
-            <button onClick={prevImage}>{"<"}</button>
-            <p>{`${currentImage + 1}/${images.length}`}</p>
-            <button onClick={nextImage}>{">"}</button>
+            <button className="carrousel-button left" onClick={prevImage}>{"<"}</button>
+            <button className="carrousel-button right" onClick={nextImage}>{">"}</button>
           </>
         )}
+        {images.length > 0 && (
+          <img src={images[currentImage]} alt={`Image ${currentImage + 1}`} className="carrousel-image" />
+        )}
+        <div className="pagination">{`${currentImage + 1}/${images.length}`}</div>
       </div>
-      {images.length > 0 && (
-        <img src={images[currentImage]} alt={`Image ${currentImage + 1}`} />
-        
-      )}
     </div>
   );
 }
 
 export default Carrousel;
+
+
+
+
+
+
 
 
 
