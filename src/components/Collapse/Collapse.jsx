1182996ea5f0de1319e3cd1da159e702534c6baf
DrissDW <driss.mallali@yahoo.com>
2023-10-05 21:04:38 +0200

    Add the Prop "pageName" to give a specific scss to the collapses of the page FicheLogement

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '../../styles/Collapse.scss';
 import iconImage from '../../assets/arrow-collapse.png'; 
 
-function Collapse({ title, content }) {
+function Collapse({ title, content, pageName }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleCollapse = () => {
@@ -12,10 +12,10 @@ function Collapse({ title, content }) {
   const iconRotation = isOpen ? 'rotate(0deg)' : 'rotate(180deg)';
 
   return (
-    <div className={`collapse ${isOpen ? 'open' : ''}`}>
-      <div className="collapse-header">
-        <div className="collapse-title">{title}</div>
-        <button className={`collapse-button ${isOpen ? 'open' : ''}`} onClick={toggleCollapse}>
+    <div className={`collapse ${isOpen ? 'open' : ''} ${pageName}-collapse`}>
+      <div className={`collapse-header ${pageName}-collapse-header`}>
+      <div className={`collapse-title ${pageName}-collapse-title`}>{title}</div>
+      <button className={`collapse-button ${isOpen ? 'open' : ''} ${pageName}-collapse-button`} onClick={toggleCollapse}>
           <img
             src={iconImage}
             alt={isOpen ? 'Icône ouverte' : 'Icône fermée'}
@@ -26,9 +26,10 @@ function Collapse({ title, content }) {
           />
         </button>
       </div>
-      <div className="collapse-content">
-        {content}
-      </div>
+      <div className={`collapse-content ${pageName}-collapse-content`}>
+  <p>{content}</p>
+</div>
+
     </div>
   );
 }
