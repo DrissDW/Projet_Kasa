5f6901081920fc0d4b0877c52c5e86e97c6c2f2d
DrissDW <driss.mallali@yahoo.com>
2023-10-05 12:47:30 +0200

    Import and add chevron_left and right icons to the Carroussel component

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import '../../styles/Carrousel.scss';
+import iconImageLeft from '../../assets/chevron_left.png';
+import iconImageRight from '../../assets/chevron_right.png';  
 
 function Carrousel({ images }) {
   const [currentImage, setCurrentImage] = useState(0);
@@ -15,10 +17,10 @@ function Carrousel({ images }) {
   return (
     <div className="carrousel">
       <div className="carrousel-image-container">
-        {images.length > 1 && ( // Condition pour afficher les boutons seulement s'il y a plus d'une image
+        {images.length > 1 && ( // Condition pour afficher les boutons et la pagination seulement s'il y a plus d'une image
           <>
-            <button className="carrousel-button left" onClick={prevImage}>{"<"}</button>
-            <button className="carrousel-button right" onClick={nextImage}>{">"}</button>
+            <button className="carrousel-button left" onClick={prevImage}>{<img src={iconImageLeft} alt='boutton Left'></img>}</button>
+            <button className="carrousel-button right" onClick={nextImage}>{<img src={iconImageRight} alt='bouton Right'></img>}</button>
             <div className="pagination">{`${currentImage + 1}/${images.length}`}</div>
           </>
         )}
